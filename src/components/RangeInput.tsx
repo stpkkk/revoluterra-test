27de50a9215cdcf88bbd1d60284aa5d90f596561
Igor Stepanov <stipyk1309@gmail.com>
2025-02-20 16:33:43 +0300

    fix: RangeInput thumb color 2

--- src/components/RangeInput.tsx
+++ src/components/RangeInput.tsx
@@ -25,9 +25,10 @@ export default function RangeInput({
 }: RangeInputProps) {
   return (
     <input
-      className={`h-4 w-full cursor-pointer appearance-none rounded-full bg-[#dcdde5] [&::-moz-range-thumb]:h-7 [&::-moz-range-thumb]:w-7 [&::-moz-range-thumb]:appearance-none [&::-moz-range-thumb]:rounded-full [&::-moz-range-thumb]:border-[6px] [&::-moz-range-thumb]:bg-white [&::-webkit-slider-thumb]:h-7 [&::-webkit-slider-thumb]:w-7 [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:border-[6px] [&::-webkit-slider-thumb]:bg-white [&::-webkit-slider-thumb]:border-${thumbColor}`}
+      className={`h-4 w-full cursor-pointer appearance-none rounded-full bg-[#dcdde5] [&::-moz-range-thumb]:h-7 [&::-moz-range-thumb]:w-7 [&::-moz-range-thumb]:appearance-none [&::-moz-range-thumb]:rounded-full [&::-moz-range-thumb]:border-[6px] [&::-moz-range-thumb]:bg-white [&::-webkit-slider-thumb]:h-7 [&::-webkit-slider-thumb]:w-7 [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:border-[6px] [&::-webkit-slider-thumb]:bg-white`}
       style={{
         background: `linear-gradient(to right, ${trackGradientColorStart} 0%, ${trackGradientColorEnd} 100%, #dcdde5 100%, #dcdde5 100%)`,
+        ['--thumb-color' as string]: thumbColor,
       }}
       value={level}
       onChange={handleChange}
